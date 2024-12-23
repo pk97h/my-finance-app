a111be4bdb9105d4f7bd2e349be38f9de1c10426
pk <yellowdayo@naver.com>
2024-12-23 15:59:30 +0900

    Home-UI 구현

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,5 +1,28 @@
+import {
+  Expense,
+  ExpenseDateTitle,
+  ExpenseListContainer,
+} from "../styles/HomeStyles";
+
 const ExpenseList = () => {
-  return <div>ExpenseList</div>;
+  return (
+    <ExpenseListContainer>
+      <Expense>
+        <ExpenseDateTitle>
+          <p>2024-01-01</p>
+          <p>햄버거</p>
+        </ExpenseDateTitle>
+        <p>2000원</p>
+      </Expense>
+      <Expense>
+        <ExpenseDateTitle>
+          <p>2024-01-01</p>
+          <p>햄버거</p>
+        </ExpenseDateTitle>
+        <p>2000원</p>
+      </Expense>
+    </ExpenseListContainer>
+  );
 };
 
 export default ExpenseList;
--- src/styles/HomeStyles.jsx
+++ src/styles/HomeStyles.jsx
@@ -35,32 +35,62 @@ export const CreateExpenseContainer = styled.div`
   border-radius: 15px;
   width: 50%;
   margin: 40px auto;
+  align-items: flex-end;
 `;
 
-export const ExpenseFormContainer = styled.form`
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-end;
-    gap:10px;
-`
-
 export const ExpenseForm = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    flex: 1 1 0%;
-    
-`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  flex: 1 1 0%;
+`;
 
 export const ExpenseFormInput = styled.input`
-border: 1px solid rgb(221, 221, 221);
-  border-radius: 8px;
-  width:100px
-`
+  border: 1px solid rgb(221, 221, 221);
+  border-radius: 4px;
+  padding: 8px;
+  width: 90%;
+  font-size: 14px;
+`;
 
 export const ExpenseFormButton = styled.button`
-  background-color:rgb(0, 123, 255);
-  color:white;
+  background-color: rgb(0, 123, 255);
+  color: white;
   border: none;
   border-radius: 8px;
+  padding: 8px;
+  cursor: pointer;
+`;
+
+export const ExpenseListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 15px;
+  width: 50%;
+  margin: 40px auto;
+`;
+
+export const Expense = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  background-color: rgb(249, 249, 249);
+  border:none;
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px;
+  cursor: pointer;
+  color: rgb(0, 123, 255);
+  font-weight: bold;
+`;
+
+export const ExpenseDateTitle = styled.div`
+  display:flex;
+  align-items:start;
+  flex-direction: column;  
 `
